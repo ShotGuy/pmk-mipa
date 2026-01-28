c3c60ae737004c08e0f275a07f6469b33cb51d86
Rio Pellokila <akdohawu@gmail.com>
2026-01-28 20:11:47 +0800

    feat(jenis-kegiatan): membuat CRUD di jenis-kegiatan

--- app/(protected)/admin/jenis-kegiatan/page.tsx
+++ app/(protected)/admin/jenis-kegiatan/page.tsx
@@ -1,7 +1,5 @@
 import { db } from "@/lib/db";
-import { AdminPageHeader } from "@/components/admin/AdminPageHeader";
-import { DataTable } from "@/components/admin/DataTable";
-import { columns } from "./columns";
+import { JenisKegiatanClient } from "@/components/admin/jenis-kegiatan/JenisKegiatanClient";
 
 export default async function JenisKegiatanPage() {
     const data = await db.jenisKegiatan.findMany({
@@ -9,18 +7,6 @@ export default async function JenisKegiatanPage() {
     });
 
     return (
-        <div className="space-y-6">
-            <AdminPageHeader
-                title="Master Jenis Kegiatan"
-                addLabel="Tambah Jenis"
-                href="/admin/jenis-kegiatan/new"
-            />
-
-            <DataTable
-                columns={columns}
-                data={data}
-                searchKey="nama"
-            />
-        </div>
+        <JenisKegiatanClient data={data} />
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         className={`${playfair.variable} ${inter.variable} font-sans antialiased bg-background text-foreground flex flex-col min-h-screen`}
       >
         {children}
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   );
